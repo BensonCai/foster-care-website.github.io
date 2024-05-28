316527bcc66c8425f08145597e825d3348d92cd2
BensonCai <bensonca@buffalo.edu>
2024-05-28 12:17:04 -0400

    uppdate case order

--- foster-care/src/Cases/navigation.jsx
+++ foster-care/src/Cases/navigation.jsx
@@ -26,9 +26,9 @@ function Navigation() {
                     <li><Link to="/home">Home</Link></li>
                     {/* Conditionally render based on the state value */}
                     {caseValue === '0' && <li><Link to="/democase">Referral Information and Assessments</Link></li>}
-                    {caseValue === '1' && <li><Link to="/case1pdfs">Referral Information and Assessments</Link></li>}
-                    {caseValue === '2' && <li><Link to="/case2pdfs">Referral Information and Assessments</Link></li>}
-                    {caseValue === '3' && <li><Link to="/case3pdfs">Referral Information and Assessments</Link></li>}
+                    {caseValue === '1' && <li><Link to="/case3pdfs">Referral Information and Assessments</Link></li>}
+                    {caseValue === '2' && <li><Link to="/case1pdfs">Referral Information and Assessments</Link></li>}
+                    {caseValue === '3' && <li><Link to="/case2pdfs">Referral Information and Assessments</Link></li>}
                     {/*  */}
                     <li><Link to="/narrative">Case Summary</Link></li>
                     <li><Link to="/forms">Treatment Plan</Link></li>
